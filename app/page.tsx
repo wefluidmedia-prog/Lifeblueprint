65ca049aa415869a3b18b31ebdf662bcfe1f3066
Fabian <wefluidmedia@gmail.com>
2026-02-17 15:51:01 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">
+        Life Blueprint – Coming Soon
+      </h1>
+    </div>
+  );
+}
